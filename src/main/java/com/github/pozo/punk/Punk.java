d86aa349b3754e759e355e6fe082d06f0d51e50f
Zoltan Polgar <pozo@gmx.com>
2016-07-17 22:56:34 +0200

    decode part

--- src/main/java/com/github/pozo/punk/Punk.java
+++ src/main/java/com/github/pozo/punk/Punk.java
@@ -1,13 +1,9 @@
 package com.github.pozo.punk;
 
-import com.github.pozo.punk.png.PNGChunk;
-import com.github.pozo.punk.png.PNGChunkList;
-import com.github.pozo.punk.png.PNGReader;
-import com.github.pozo.punk.png.PNGWriter;
+import com.github.pozo.punk.png.*;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.Iterator;
 
 /**
  * Created by pozo on 2016.07.14..
@@ -33,7 +29,34 @@ public class Punk {
         pngWriter.writeFully(bytes);
     }
 
-    public void decode(InputStream source, OutputStream target) throws IOException {
+    public void decode(InputStream targetPNG, OutputStream target) throws IOException {
+        final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(target);
+
+        final PNGReader png = new PNGReader(targetPNG);
+        final PNGChunkList pngChunks = png.readChunks();
+
+        boolean hasPunkChunk = false;
+
+        final Iterator<PNGChunkHolder> iterator = pngChunks.iterator();
+
+        while(iterator.hasNext() && !hasPunkChunk) {
+            final PNGChunkHolder pngChunk = iterator.next();
+
+            final String type = pngChunk.getPngChunk().getType();
+            final byte[] data = pngChunk.getPngChunk().getData();
+
+            if(CHUNK_NAME.equals(type)) {
+                bufferedOutputStream.write(data);
+                bufferedOutputStream.close();
+
+                hasPunkChunk = true;
+            }
+        }
+
+        if(!hasPunkChunk) {
+            bufferedOutputStream.close();
+            throw new IOException("puNk chunk is not found");
+        }
 
     }
 }
